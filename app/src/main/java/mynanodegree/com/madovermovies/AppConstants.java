e1cc09e71befcd837b9007bfc1695874828f23f3
Arjun-27 <arjun.chouhan68@gmail.com>
2016-04-23 10:58:36 +0530

    API Key

--- app/src/main/java/mynanodegree/com/madovermovies/AppConstants.java
+++ app/src/main/java/mynanodegree/com/madovermovies/AppConstants.java
@@ -4,7 +4,7 @@ package mynanodegree.com.madovermovies;
  * Created by Arjun Chouhan on 29-02-2016.
  */
 public class AppConstants {
-    private static final String API_KEY = "f2a6464871afd402eb9235b937f98926";
+    private static final String API_KEY = "";
 
     public static final String BASE_IMAGE_PATH = "http://image.tmdb.org/t/p/w342";
     public static final String BASE_THUMB_PATH = "http://img.youtube.com/vi/**/default.jpg";
